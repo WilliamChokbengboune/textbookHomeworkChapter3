06e2803b7cc0cc543d2a2d8885cc8ebc5f695f0e
willc <wchokbengboune@ggc.edu>
2023-02-09 16:41:39 -0500

    Comments have been added to make it more descriptive Variables have been made more descriptive

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,7 +1,7 @@
 /**
  * Class: Triangle
- * Author:
- * Written:
+ * Author: William Chokbengboune
+ * Written: 2/9/2023
  * Version: 1.0
  * Description: This program checks if any 2 added sides of a triangle are greater one size.
  * If the 2 sides are not greater than, it is not a triangle.
@@ -12,27 +12,31 @@ import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter edge A: ");
-        int a = in.nextInt();
-        System.out.println("Enter edge B: ");
-        int b = in.nextInt();
-        System.out.println("Enter edge C: ");
-        int c = in.nextInt();
-        if (a + b > c){
-            if (b + c > a){
-                if (a + c > b){
+        System.out.println("Enter the length of edge a: ");
+        double lengthA = in.nextDouble();
+        System.out.println("Enter the length of edge B: ");
+        double lengthB = in.nextDouble();
+        System.out.println("Enter the length of edge C: ");
+        double lengthC = in.nextDouble();
+        if (lengthA + lengthB > lengthC){
+            if (lengthB + lengthC > lengthA){
+                if (lengthA + lengthC > lengthB){
                     System.out.println("valid");
+                    //if any 2 sides added aare greater than the remaining side then it is valid
                 }
                 else{
-                    System.out.println("invalid");
+                    System.out.println("Invalid input");
+                    //Only works if perimeter of a and c is greater than b
                 }
             }
             else{
-                System.out.println("Invalid");
+                System.out.println("Invalid input");
+                //only works if b and c added is greater than a
             }
         }
         else{
-            System.out.println("Invalid");
+            System.out.println("Invalid input");
+            //only works if a and b added together is greater than c
         }
     }
 }
